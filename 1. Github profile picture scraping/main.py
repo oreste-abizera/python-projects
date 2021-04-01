f867c35b104d151f54cf7a2caa05e075f607ba2b
oreste-abizera <oresteabizera11@gmail.com>
2021-04-01 18:07:46 +0200

    Project 1: Extract profile image from request received

--- 1. Github profile picture scraping/main.py
+++ 1. Github profile picture scraping/main.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-github_user = input("Input Github user: ")
-r = requests.get("https://github.com/"+github_user)
+githubUser = input("Input Github user: ")
+r = requests.get("https://github.com/"+githubUser)
+
+soup = bs(r.content, "html.parser")
+profileImageUrl = soup.find("img", { "alt" : "Avatar" })["src"]
+print(profileImageUrl)
