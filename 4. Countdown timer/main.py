b853a2c423ec1d0fb2ec6e42fe8dbf51aa974264
oreste-abizera <oresteabizera11@gmail.com>
2021-04-06 18:33:27 +0200

    \r paramater usage

--- 4. Countdown timer/main.py
+++ 4. Countdown timer/main.py
@@ -4,7 +4,7 @@ def countdown(t):
     while t:
         mins,secs = divmod(t,60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer)
+        print(timer, end="\r")
         time.sleep(1)
         t -= 1
 
