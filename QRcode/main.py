ba54b757a4200fa0db7f1ebfe46c04102c5ce47e
oreste-abizera <oresteabizera11@gmail.com>
2021-04-06 19:17:03 +0200

    Customizing qrcode

--- QRcode/main.py
+++ QRcode/main.py
@@ -2,6 +2,18 @@ import qrcode
 
 data = "Names: ABIZERA Oreste"
 
-img = qrcode.make(data)
+# this is the default QRCode
+# img = qrcode.make(data)
+
+
+# customizing qrcode
+
+qr = qrcode.QRCode(version=1, box_size=10, border=5)
+
+qr.add_data(data)
+
+qr.make(fit=True)
+
+img = qr.make_image(fill_color: 'red', back_color: 'white')
 
 img.save("C:/Users/DELL/Desktop/Projects/Python/Python projects/QRcode/TestFolder/myQRcode.png")
