83a942560fb90463f018f5378a194075da269ac9
oreste-abizera <oresteabizera11@gmail.com>
2021-04-06 18:26:30 +0200

    Countdown function and using sleep from time

--- 4. Countdown timer/main.py
+++ 4. Countdown timer/main.py
@@ -1,1 +1,11 @@
 import time
+
+def countdown(t):
+    while t:
+        mins,secs = divmod(t,60)
+        print(mins,secs)
+        time.sleep(1)
+        t -= 1
+
+t = input("Enter time in seconds: ")
+countdown(int(t))
