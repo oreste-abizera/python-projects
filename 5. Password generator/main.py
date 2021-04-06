a2c6ee0130dce8aece8eb640934914f499a34600
oreste-abizera <oresteabizera11@gmail.com>
2021-04-06 18:59:47 +0200

    Finshing Password generator program

--- 5. Password generator/main.py
+++ 5. Password generator/main.py
@@ -1,3 +1,20 @@
 import random
 
+print("\nWelcome to Password generator\n")
+
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-,.?<>:|0123456789'
+
+num = input("How many passwords to generate: ")
+num = int(num)
+
+length = input("How many characters consisting the password: ")
+length = int(length)
+
+print("\nList of generated passwords:\n")
+
+for pwd in range(num):
+    password = ''
+    for c in range(length):
+        password += random.choice(chars)
+
+    print(str(pwd+1) + ". " + password)
